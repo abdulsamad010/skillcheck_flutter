75651485965d3a9a71b71b37b520b6dc6e1f703d
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-20 06:45:46 +0500

    Implement local database helper

---
+++ lib/core/database/database_helper.dart
@@ -0,0 +1,79 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class databaseHelper{
+  Database? db;
+
+  Future<void> createDb() async{
+
+    if(db!=null){
+      return;
+    }
+
+    db=await openDatabase(
+      join(await getDatabasesPath(),'skillcheck.db'),
+      version: 1,
+      onCreate: (db,version) async{
+        await db.execute(
+          '''
+          CREATE TABLE PROFILE(
+          NAME TEXT NOT NULL,
+          EDUCATION TEXT NOT NULL,
+          PROFESSION TEXT NOT NULL
+          )
+          '''
+        );
+
+        await db.execute(
+            '''
+          CREATE TABLE QUIZ(
+          QUIZ_NO INTEGER PRIMARY KEY AUTOINCREMENT,
+          CATEGORY_NAME TEXT NOT NULL,
+          SKILL_NAME TEXT NOT NULL,
+          MARKS INTEGER NOT NULL,
+          IS_PASS INTEGER NOT NULL
+          )
+          '''
+        );
+      }
+    );
+  }
+
+  Future<void> insertProfile(String name,String education,String profession) async{
+    await createDb();
+    await db!.insert('PROFILE', {
+      'NAME':name,
+      'EDUCATION': education,
+      'PROFESSION': profession,
+    });
+  }
+
+  Future<void> insertQuiz(String categoryName, String skillName, int marks, int isPass,) async {
+    await createDb();
+    await db!.delete('PROFILE');
+    await db!.insert(
+      'QUIZ',
+      {
+        'CATEGORY_NAME': categoryName,
+        'SKILL_NAME': skillName,
+        'MARKS': marks,
+        'IS_PASS': isPass,
+      },
+    );
+  }
+
+  Future<void> updateProfile(String name,String education,String profession) async{
+    await createDb();
+    await db!.update('PROFILE', {
+      'NAME':name,
+      'EDUCATION': education,
+      'PROFESSION': profession,
+    });
+  }
+
+  Future<void> deleteProfile() async{
+    await createDb();
+    await db!.delete('PROFILE');
+    await db!.delete('Quiz');
+  }
+}
