0f2c145f663805e6a1887ec24851edede29e2c81
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-22 04:34:19 +0500

    update app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:skillcheck_flutter/features/home/home_screen.dart';
+import 'features/personal_information/personal_information_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   colorScheme:  ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
 
-      home: HomeScreen(),
+      home: PersonalInformationScreen(),
     );
   }
 }
