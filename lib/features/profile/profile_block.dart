6ce79744d994a5487930cbdc1d2e5de5c11d745c
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-22 04:34:15 +0500

    update profile bloc

---
+++ lib/features/profile/profile_block.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:skillcheck_flutter/core/database/database_helper.dart';
+import 'package:skillcheck_flutter/features/profile/profile_event.dart';
+import 'package:skillcheck_flutter/features/profile/profile_state.dart';
+
+class ProfileBlock extends Bloc<ProfileEvent,ProfileState>{
+  ProfileBlock() : super(ProfileState()){
+    on<InitializeProfile>((event,emit) async{
+
+      final List<Map<String,dynamic>> newProfile= await DatabaseHelper.readProfile();
+      final List<Map<String,dynamic>> newQuiz=await DatabaseHelper.readQuiz();
+
+      print(newProfile);
+      print(newQuiz);
+
+      if(!newQuiz.isEmpty && !newQuiz.isEmpty) {
+        emit(
+            ProfileState(
+                name: newProfile[0]["NAME"],
+                education: newProfile[0]["EDUCATION"],
+                profession: newProfile[0]["PROFESSION"],
+
+                quiz: newQuiz
+            )
+        );
+      }
+    });
+  }
+}
