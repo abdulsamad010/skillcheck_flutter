917bf5caa3ad02e5ef9b4926ae8d370be35e78db
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-21 05:37:38 +0500

    Update quiz state

--- lib/features/quiz/quiz_state.dart
+++ lib/features/quiz/quiz_state.dart
@@ -1,7 +1,11 @@
 class QuizState {
   final int marks,currentQuestion;
   final int? selectedIndex;
-  final String selectedSkill;
+  final String selectedSkill,mainSelectedSkill;
   final bool isEndQuiz;
-  QuizState({this.marks=0,this.currentQuestion=0,this.selectedIndex,required this.selectedSkill,this.isEndQuiz=false});
+  final String question;
+  final String answer;
+  final List<String> options;
+  final int? isPass;
+  QuizState({this.marks=0,this.currentQuestion=0,this.selectedIndex,required this.selectedSkill,this.isEndQuiz=false,this.question='',this.answer='',this.options=const[],this.isPass=null,required this.mainSelectedSkill});
 }
