438835826bdc51f1a5dbac2dbe9314fc09e61ae9
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-22 04:34:17 +0500

    update skill categories

--- lib/features/skill_category/skill_data.dart
+++ lib/features/skill_category/skill_data.dart
@@ -4,16 +4,19 @@ final Map<String, List<String>> skillCategories = {
     "Object-Oriented Programming",
     "Data Structures & Algorithms",
   ],
+
   "Computer Skills": [
     "Computer Fundamentals",
     "Operating Systems",
     "Internet & Web Basics",
   ],
+
   "Digital & Cybersecurity Skills": [
     "Cybersecurity Basics",
     "Online Safety",
     "Password & Privacy Awareness",
   ],
+
   "Problem Solving Skills": [
     "Logical Reasoning",
     "Analytical Thinking",
