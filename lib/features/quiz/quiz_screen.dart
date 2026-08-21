95211f73d73ad1a11cb64ab7f327d7687d9dd229
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-21 05:38:26 +0500

    Integrate quiz screen with bloc

--- lib/features/quiz/quiz_screen.dart
+++ lib/features/quiz/quiz_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skillcheck_flutter/core/widgets/custom_app_Bar.dart';
+import 'package:skillcheck_flutter/features/quiz/quiz_bloc.dart';
 import 'package:skillcheck_flutter/features/quiz/quiz_data.dart';
+import 'package:skillcheck_flutter/features/quiz/quiz_event.dart';
+import 'package:skillcheck_flutter/features/quiz/quiz_state.dart';
 
 class QuizScreen extends StatefulWidget {
-  String name;
-  QuizScreen({super.key,required this.name});
+  QuizScreen({super.key});
 
   @override
   State<QuizScreen> createState() => _QuizScreenState();
@@ -13,48 +16,62 @@ class QuizScreen extends StatefulWidget {
 
 class _QuizScreenState extends State<QuizScreen> {
 
-  int marks=0,currentQuestion=0;
-  int? selectedIndex=null;
-
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: CustomAppBar(name: "Quiz For ${widget.name}"),
+      appBar: CustomAppBar(name: "Quiz For ${context.read<QuizBloc>().state.selectedSkill}"),
       body:Column(
         children: [
 
           Text("Be Careful! Correct Answer Give You Two Marks And Wrong Answer Will Deduct Two Marks"),
           
-          Text("Question No ${currentQuestion+1}: ${quizzes[widget.name][currentQuestion]['question']}"),
+
+          BlocBuilder<QuizBloc,QuizState>(builder: (context,state){
+            return Text("Question No ${state.currentQuestion+1}: ${state.question}");
+          }),
 
           Text("Options:"),
 
           Expanded(
-            child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            child:  BlocBuilder<QuizBloc,QuizState>(builder: (context,state){
+              return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
                 itemCount: 4,
                 itemBuilder: (context,index){
               return InkWell(
                   onTap: (){
-                    selectedIndex=index;
+                    context.read<QuizBloc>().add(SelectOption(index));
                   },
-                  child: Text("Option ${index+1}: ${quizzes[widget.name][currentQuestion]['options'][index]}"));
-                }),
-          ),
+                  child: Text("Option ${index+1}: ${state.options[index]}")
 
-          InkWell(onTap: (){
+                             );},);}
+          ),),
 
-            if(selectedIndex==null){
-              return;
+          InkWell(onTap: () {
+            context.read<QuizBloc>().add(NextQuestion());
+
+            final state = context.read<QuizBloc>().state;
+
+            if (state.isEndQuiz) {
+              context.read<QuizBloc>().add(StoreResults());
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (_) => AlertDialog(
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text("Quiz Results"),
+                      Text("Obtained Marks: ${state.marks}"),
+                      const Text("Total Marks: 20"),
+                      Text("Pass or Fail: ${state.isPass}"),
+                    ],
+                  ),
+                ),
+              );
             }
+          },child: Text("Next"),)
 
-
-
-
-
-            },
-          child: Text("Next"),
-          )
         ],
       )
     );
