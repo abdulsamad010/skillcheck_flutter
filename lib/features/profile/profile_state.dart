e17ee06e2bc29836d38f209256c124c22258cf15
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-22 04:34:16 +0500

    update profile state

---
+++ lib/features/profile/profile_state.dart
@@ -0,0 +1,7 @@
+class ProfileState {
+  final String? name,education,profession;
+
+  final List<Map<String,dynamic>>? quiz;
+
+  ProfileState({this.name,this.education,this.profession,this.quiz});
+}
