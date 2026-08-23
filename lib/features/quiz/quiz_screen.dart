7b94be2e54bae78448eed13b857b49d396ce4a5b
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-24 00:23:05 +0500

    update quiz screen

--- lib/features/quiz/quiz_screen.dart
+++ lib/features/quiz/quiz_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skillcheck_flutter/core/widgets/custom_app_Bar.dart';
 import 'package:skillcheck_flutter/features/home/home_screen.dart';
 import 'package:skillcheck_flutter/features/quiz/quiz_bloc.dart';
+import 'package:skillcheck_flutter/features/quiz/quiz_end_screen.dart';
 import 'package:skillcheck_flutter/features/quiz/quiz_event.dart';
 import 'package:skillcheck_flutter/features/quiz/quiz_state.dart';
 
@@ -15,100 +16,150 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+
+  List<Color> optionColor=[Colors.grey,Colors.grey,Colors.grey,Colors.grey];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+    final height=MediaQuery.sizeOf(context).height;
+    final width=MediaQuery.sizeOf(context).width;
+
+    return BlocListener<QuizBloc,QuizState>(listener:(context,state) async{
+
+      if (context.read<QuizBloc>().state.isEndQuiz && context.read<QuizBloc>().state.isPass == null) {
+        context.read<QuizBloc>().add(StoreResults());
+      }
+
+      CircularProgressIndicator();
+      await Future.delayed(Duration(
+        seconds: 5
+      ));
+
+      if (context.read<QuizBloc>().state.isPass != null) {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>QuizEndScreen()));
+
+
+      }
+
+    },
+
+    child:  Scaffold(
       appBar: CustomAppBar(
         name: "Quiz For ${context.read<QuizBloc>().state.selectedSkill}",
       ),
-      body: BlocListener<QuizBloc, QuizState>(
-        listener: (context, state) {
+      body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
 
-          if (state.isEndQuiz && state.isPass == null) {
-            context.read<QuizBloc>().add(StoreResults());
-          }
+                  SizedBox(height:height*0.03),
 
-          if (state.isPass != null) {
-            showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (_) => AlertDialog(
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                   Text("Quiz Results"),
-                    Text("Obtained Marks: ${state.marks}"),
-                    Text("Total Marks: 20"),
-                    Text("Pass or Fail: ${state.isPass}"),
-                  ],
+                Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.orangeAccent,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black,blurRadius: 8)
+                    ]
                 ),
-                actions: [
-                  ElevatedButton(onPressed: (){
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
-                  }, child: Text("Go To Home"))
+                padding: EdgeInsets.all(height*0.02),
+                child:Text(
+                  "Be careful! A correct answer gives you 2 marks, "
+                      "while a wrong answer deducts 2 marks.",
+                style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.bold),),),
+
+                  SizedBox(height:height*0.05),
+
+                  BlocBuilder<QuizBloc, QuizState>(
+                    builder: (context, state) {
+                      return Text(
+                        "Question No ${state.currentQuestion + 1}: "
+                            "${state.question}",style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),
+                      );
+                    },
+                  ),
+
+                  SizedBox(height:height*0.03),
+
+                  BlocBuilder<QuizBloc, QuizState>(
+                    builder: (context, state) {
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          childAspectRatio: 2,
+                          crossAxisCount: 2,
+                        ),
+                        itemCount: state.options.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: EdgeInsets.all(8),
+                            margin: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: optionColor[index],
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: Material(
+                              color: optionColor[index],
+                              child: InkWell(
+                                splashColor: Colors.deepPurple,
+                                borderRadius: BorderRadius.circular(25),
+                                onTap: () {
+
+                                  optionColor=[Colors.grey,Colors.grey,Colors.grey,Colors.grey,];
+                                  optionColor[index]=Colors.orangeAccent;
+
+                                  context
+                                      .read<QuizBloc>()
+                                      .add(SelectOption(index));
+                                },
+                                child: Column(
+                                  children: [
+                                    Expanded(child: Text("Option ${index + 1}", style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold))),
+
+                                    Expanded(child: Text("${state.options[index]}", style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),)),
+
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+
+                  SizedBox(height:height*0.05),
+
+              Center(
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple
+                    ),
+                    onPressed: (){
+                      optionColor=[Colors.grey,Colors.grey,Colors.grey,Colors.grey];
+
+                      context.read<QuizBloc>().add(NextQuestion());
+
+
+
+
+                           }, child:  Text("Next Question",style: TextStyle(color: Colors.white),),
+                )),
+
+                  Align(
+                  alignment: AlignmentGeometry.bottomRight,
+                  child: Text("Total Questions: 10",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: Colors.grey),))
+
+
                 ],
               ),
-            );
-          }
-        },
-
-        child: SafeArea(
-          child: Column(
-            children: [
-
-              Text(
-                "Be Careful! Correct Answer Give You Two Marks "
-                    "And Wrong Answer Will Deduct Two Marks",
-              ),
-
-              BlocBuilder<QuizBloc, QuizState>(
-                builder: (context, state) {
-                  return Text(
-                    "Question No ${state.currentQuestion + 1}: "
-                        "${state.question}",
-                  );
-                },
-              ),
-
-              Text("Options:"),
-
-              Expanded(
-                child: BlocBuilder<QuizBloc, QuizState>(
-                  builder: (context, state) {
-                    return GridView.builder(
-                      gridDelegate:
-                      const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                      ),
-                      itemCount: state.options.length,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            context
-                                .read<QuizBloc>()
-                                .add(SelectOption(index));
-                          },
-                          child: Text(
-                            "Option ${index + 1}: "
-                                "${state.options[index]}",
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-
-              InkWell(
-                onTap: () {
-                  context.read<QuizBloc>().add(NextQuestion());
-                },
-                child: Text("Next"),
-              ),
-            ],
-          ),
+            ),
         ),
-      ),
-    );
-  }
+      ));}
 }
